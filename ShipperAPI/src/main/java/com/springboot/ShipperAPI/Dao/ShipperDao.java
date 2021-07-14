318fffc60bcdbac2c71546b3e252e0f58c8eccb1
Sritam <sritambehera110@gmail.com>
2021-07-14 18:12:35 +0530

    Changed response if phone no exists in db

--- ShipperAPI/src/main/java/com/springboot/ShipperAPI/Dao/ShipperDao.java
+++ ShipperAPI/src/main/java/com/springboot/ShipperAPI/Dao/ShipperDao.java
@@ -1,6 +1,8 @@
 package com.springboot.ShipperAPI.Dao;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,13 +10,13 @@ import com.springboot.ShipperAPI.Entity.Shipper;
 
 public interface ShipperDao extends JpaRepository<Shipper, String> {
 	@Query("select phoneNo from Shipper s where s.phoneNo = :phoneNo")
-	Long findByPhoneNo(String phoneNo);	
-	
+	public Long findByPhoneNo(String phoneNo);	
+
 	@Query("select s from Shipper s where s.phoneNo = :phoneNo")
-	Shipper findShipperByPhoneNo(String phoneNo);	
+	public Optional<Shipper> findShipperByPhoneNo(String phoneNo);	
 
 	@Query("select s from Shipper s")
-	List<Shipper> getAll(Pageable pageable);
-	
-	List<Shipper> findByCompanyApproved(Boolean companyApproved, Pageable pageable);
+	public List<Shipper> getAll(Pageable pageable);
+
+	public List<Shipper> findByCompanyApproved(Boolean companyApproved, Pageable pageable);
 }
